be6540ebfeb2870418d86d87a0da4aa175f5e4a1
Louis Jeanes <81524093+louboi@users.noreply.github.com>
2024-11-18 12:02:30 +0000

    S

--- Security.py
+++ Security.py
@@ -1,5 +1,6 @@
 # Import the required functions
 import sqlite3 # Import the SQLite Function used to manage the database
+import math
 
 # Connect to the database
 con = sqlite3.connect("Secrets.db")
@@ -9,18 +10,24 @@ cur = con.cursor()
 UUID = []
 result = 0
 Checksum = 0
+x = 0
+y = 0
+z = 0
 
 # Check the Username and Password are valid
 def check_validity(username, password):
     if len(username) > 10:
         print("Username invalid (001)")
+        quit()
     elif len(username) == 0:
         print("Username invalid (002)")
+        quit()
     if len(password) > 10:
         print("Username invalid (001)")
+        quit()
     elif len(password) == 0:
         print("Username invalid (002)")
-    quit()
+        quit()
 
 # Generate the UUID by converting to ascii decimal values and appending to a list.
 def UUID_gen(password, username, UUID, result):
@@ -35,18 +42,32 @@ def UUID_gen(password, username, UUID, result):
     print(result)
 
 # Generate a checksum for validating users for entry
-def checksum_gen(UUID, Checksum):
-    if result // 2 != 0:
-        print("The number is odd.")
-    elif result // 2 == 0:
-        print("The number is even.")
+def checksum_gen(UUID, Checksum, result, x, y, z):
+    print(UUID)
+    for x in UUID:
+        z = int(UUID[x])
+    y = math.tan(((z ** 2) + (z ** 4) + (z ** 3) + (z ** 5) + (z ** 8) + (z ** 1)) ** (29/512))
+    Checksum = str(y)
+    print(Checksum)
+
+def check(username, password, valid):
+    cur.execute("SELECT UUID FROM login WHERE Username = ?", (username,))
+    z = cur.fetchall()
+    if password == str(z[0][0]):
+        print(str(z[0][0]))
+        print("You passed!")
+        return True
     else:
-        print("Error code 001")
+        print(str(z[0][0]))
+        print("The KGB got you, you no longer exist!! :)")
+        return False    
 
 # Get the user to make a Username and Password
 username = str(input("Please make a Username (less than 10 characters): "))
 password = str(input("Please make a Password (less than 10 characters): "))
 
 #----------Main Program begins here----------#
+
 check_validity(username, password) # Validate the Username and Password
 UUID_gen(password, username, UUID, result) # Generate the UUID
+checksum_gen(UUID, Checksum, result, x, y, z) # Generate a checksum
